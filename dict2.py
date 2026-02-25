0c93bd1b3aa6c8c74263dd5bf6e90caffb4060a8
Roshan1772006 <roshankaduskar6@gmail.com>
2026-02-25 20:32:10 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,17 @@
+# dictionary
+dict1 = {
+    1:"roshan",
+    2:"rohan",
+    3:"ros",
+    4:"rishan",
+}
+print(dict1)
+
+print(dict1[1])
+
+dict1.pop(2)
+print(dict1)
+print(dict1.values())
+print(dict1.keys())
+print(dict1.copy())
+print(dict1.items())
